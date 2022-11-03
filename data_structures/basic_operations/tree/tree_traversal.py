07ac3539faa74e219483b501b12e1a164a0f3b8c
Piyush Makhija <piyushmakhija5@gmail.com>
2022-11-03 11:14:21 +0530

    Update for all recursive tree traversals

--- data_structures/basic_operations/tree/tree_traversal.py
+++ data_structures/basic_operations/tree/tree_traversal.py
@@ -9,6 +9,16 @@ class Node:
         print(root.val)
         print_inOrder(root.right)
 
+    def print_preOrder(self, root):
+        print(root.val)
+        print_preOrder(root.left)
+        print_preOrder(root.right)
+
+    def print_postOrder(self, root):
+        print_postOrder(root.left)
+        print_postOrder(root.right)
+        print(root.val)
+
     if __name__ == "__main__":
         root = Node(1)
         root.left = Node(2)
@@ -17,3 +27,5 @@ class Node:
         root.right.right = Node(5)
 
         print_inOrder(root)
+        print_preOrder(root)
+        print_postOrder(root)
