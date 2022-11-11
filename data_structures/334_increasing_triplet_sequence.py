933ed584d2df37c6afb27f2c2b67245a7d951244
Piyush Makhija <piyushmakhija5@gmail.com>
2022-11-11 11:35:57 +0530

    Create 334_increasing_triplet_sequence.py

---
+++ data_structures/334_increasing_triplet_sequence.py
@@ -0,0 +1,13 @@
+## https://leetcode.com/problems/increasing-triplet-subsequence/
+
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        first, second = inf, inf
+        for third in nums:
+            if second<third:
+                return True
+            if third <= first:
+                first = third
+            else:
+                second = third
+        return False
