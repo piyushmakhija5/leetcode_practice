2ae5474b494a057794bb2d7c0fbdd0e44df96450
Piyush Makhija <piyushmakhija5@gmail.com>
2022-11-12 12:25:01 +0530

    Update and rename 876_middle_of_the_linked_list to 876_middle_of_the_linked_list.py

--- data_structures/876_middle_of_the_linked_list.py
+++ data_structures/876_middle_of_the_linked_list.py
@@ -6,6 +6,7 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         ## Fast solution: Only 1 traversal
